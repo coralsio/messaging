1a48807027a162283b3dae8ef1c78018936b94c8
Ahmed Thyab <ahmedoh.thiab@gmail.com>
2024-06-25 13:34:45 +0300

    channels authentication

--- src/routes/channels.php
+++ src/routes/channels.php
@@ -5,8 +5,8 @@ use Corals\User\Models\User;
 use Corals\User\Transformers\API\SimpleUserPresenter;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('messages.{user}', function (User $user) {
-    return true;
+Broadcast::channel('messages.{user}', function (User $authUser, User $user) {
+    return $authUser->id === $user->id;
 });
 
 Broadcast::channel('online', function (User $user) {
@@ -14,5 +14,8 @@ Broadcast::channel('online', function (User $user) {
 });
 
 Broadcast::channel('openedDiscussion.{discussion}', function (User $user, Discussion $discussion) {
-    return true;
+    return $discussion->participations()->where([
+        'messaging_participations.participable_type' => getMorphAlias($user),
+        'messaging_participations.participable_id' => $user->id,
+    ])->exists();
 });
